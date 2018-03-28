7143a1bd766ceaf8235392ea054068a0b9521515
bgriessbach <b.griessbach@gmail.com>
2018-03-28 18:21:54 +0100

    added random userid generation added random-js dependency tested endpoint direct return as expected

--- backend/dogwalkers/api/controllers/users.js
+++ backend/dogwalkers/api/controllers/users.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const Q = require('q');
 const userinfo_c = ['walker_id', 'name', 'email', 'picture', 'creation_date'];
+const Random = require("random-js");
 
 
 module.exports = {
@@ -13,7 +14,10 @@ module.exports = {
 
 function createUser(req, res, next) {
 	let userinfo = req.swagger.params.userinfo.value;
-	res.json();
+	let r = new Random();
+	let walker_id = r.string(14);
+	console.log(walker_id);
+	res.json({user_id: walker_id});
 }
 
 function deleteUser(req, res, next) {
